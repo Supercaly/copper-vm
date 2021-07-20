aac6619dd26da34a5f33b85a7499ec6d38de104f
Lorenzo <djonline96@gmail.com>
2021-07-20 15:10:01 +0200

    Add TODO(#8)

--- cmd/casm/casm.go
+++ cmd/casm/casm.go
@@ -3,6 +3,7 @@ package main
 import c "coppervm.com/coppervm/pkg/casm"
 
 func main() {
+	// TODO(#8): Get casm program form command line arguments
 	casm := c.Casm{}
 	casm.TranslateSource("examples/123.copper")
 	casm.SaveProgramToFile("examples/bin/123.vm")
