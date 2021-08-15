7ba4eca36dc33579198aadf46af6e8b081742ee9
Lorenzo <djonline96@gmail.com>
2021-08-15 12:33:33 +0200

    Add tests to compiler

--- pkg/casm/compiler_test.go
+++ pkg/casm/compiler_test.go
@@ -1,6 +1,7 @@
 package casm
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/Supercaly/coppervm/pkg/coppervm"
@@ -71,4 +72,326 @@ func instArrayEquals(a []coppervm.InstDef, b []coppervm.InstDef) bool {
 		}
 	}
 	return true
+}
+
+func TestGetBindingByName(t *testing.T) {
+	casm := Casm{
+		Bindings: []Binding{
+			{Name: "a_label",
+				Value:    Expression{Kind: ExpressionKindNumLitInt, AsNumLitInt: 1},
+				Location: FileLocation{},
+				IsLabel:  true},
+			{Name: "a_const",
+				Value:    Expression{Kind: ExpressionKindNumLitFloat, AsNumLitFloat: 2.3},
+				Location: FileLocation{},
+				IsLabel:  false},
+		},
+	}
+
+	e, b := casm.getBindingByName("a_label")
+	if !e {
+		t.Errorf("cannot find binging with name 'a_label'")
+	}
+	if b != casm.Bindings[0] {
+		t.Errorf("expecting %#v but got %#v", casm.Bindings[0], b)
+	}
+
+	e, b = casm.getBindingByName("a_const")
+	if !e {
+		t.Errorf("cannot find binging with name 'a_const'")
+	}
+	if b != casm.Bindings[1] {
+		t.Errorf("expecting %#v but got %#v", casm.Bindings[1], b)
+	}
+
+	e, b = casm.getBindingByName("test")
+	if e {
+		t.Errorf("no binding with name 'test' should exist")
+	}
+}
+
+func TestBindLabel(t *testing.T) {
+	casm := Casm{
+		Bindings: []Binding{
+			{Name: "a_label",
+				Value:    Expression{Kind: ExpressionKindNumLitInt, AsNumLitInt: 1},
+				Location: FileLocation{},
+				IsLabel:  true},
+		},
+	}
+
+	func() {
+		defer func() { recover() }()
+		casm.bindLabel("a_label", 1, FileLocation{})
+		t.Error("Expecting an error")
+	}()
+
+	casm.bindLabel("new_label", 2, FileLocation{})
+	e, b := casm.getBindingByName("new_label")
+	if !e {
+		t.Error("the new_label was not bound correctly")
+	}
+
+	lab := Binding{
+		Name: "new_label",
+		Value: Expression{
+			Kind:        ExpressionKindNumLitInt,
+			AsNumLitInt: 2,
+		},
+		Location: FileLocation{},
+		IsLabel:  true,
+	}
+	if !reflect.DeepEqual(b, lab) {
+		t.Errorf("expecting %#v but got %#v", lab, b)
+	}
+}
+
+func TestBindConst(t *testing.T) {
+	casm := Casm{
+		Bindings: []Binding{
+			{Name: "a_const",
+				Value:    Expression{Kind: ExpressionKindNumLitInt, AsNumLitInt: 1},
+				Location: FileLocation{},
+				IsLabel:  false},
+		},
+	}
+
+	func() {
+		defer func() { recover() }()
+		casm.bindConst(DirectiveLine{Name: "const", Block: "a_const 1"},
+			FileLocation{})
+		t.Error("Expecting an error")
+	}()
+
+	casm.bindConst(DirectiveLine{Name: "const", Block: "new_const 2"},
+		FileLocation{})
+	e, b := casm.getBindingByName("new_const")
+	if !e {
+		t.Error("the new_const was not bound correctly")
+	}
+	c := Binding{
+		Name: "new_const",
+		Value: Expression{
+			Kind:        ExpressionKindNumLitInt,
+			AsNumLitInt: 2,
+		},
+		Location: FileLocation{},
+		IsLabel:  false,
+	}
+	if !reflect.DeepEqual(b, c) {
+		t.Errorf("expecting %#v but got %#v", c, b)
+	}
+
+	func() {
+		defer func() {
+			recover()
+		}()
+		casm.bindConst(DirectiveLine{Name: "const", Block: "new_const2"},
+			FileLocation{})
+		t.Error("Expecting an error")
+	}()
+}
+func TestBindEntry(t *testing.T) {
+	func() {
+		casm := Casm{}
+		defer func() { recover() }()
+		casm.HasEntry = true
+		casm.bindEntry("main", FileLocation{})
+		t.Error("expecting an error")
+	}()
+
+	casm := Casm{}
+	casm.bindEntry("entry", FileLocation{"", 10})
+	if !casm.HasEntry ||
+		casm.DeferredEntryName != "entry" ||
+		casm.EntryLocation.Location != 10 {
+		t.Error("entry location not set")
+	}
+}
+
+func TestBindMemory(t *testing.T) {
+	casm := Casm{
+		Bindings: []Binding{
+			{Name: "mem",
+				Value:    Expression{Kind: ExpressionKindNumLitInt, AsNumLitInt: 0},
+				Location: FileLocation{},
+				IsLabel:  false},
+		},
+	}
+
+	func() {
+		defer func() { recover() }()
+		casm.bindMemory(DirectiveLine{Name: "memory", Block: "mem 1"},
+			FileLocation{})
+		t.Error("Expecting an error")
+	}()
+
+	casm.bindMemory(DirectiveLine{Name: "memory", Block: "new_mem 2,3"},
+		FileLocation{})
+	e, b := casm.getBindingByName("new_mem")
+	if !e {
+		t.Error("the new_mem was not bound correctly")
+	}
+	c := Binding{
+		Name: "new_mem",
+		Value: Expression{
+			Kind:        ExpressionKindNumLitInt,
+			AsNumLitInt: 0,
+		},
+		Location: FileLocation{},
+		IsLabel:  false,
+	}
+	if !reflect.DeepEqual(b, c) {
+		t.Errorf("expecting %#v but got %#v", c, b)
+	}
+
+	func() {
+		defer func() {
+			recover()
+		}()
+		casm.bindMemory(DirectiveLine{Name: "memory", Block: "new_mem2 ,"},
+			FileLocation{})
+		t.Error("Expecting an error")
+	}()
+}
+
+func TestEvaluateExpression(t *testing.T) {
+	casm := Casm{}
+
+	w := casm.evaluateExpression(Expression{
+		Kind:        ExpressionKindNumLitInt,
+		AsNumLitInt: 10,
+	}, FileLocation{})
+	if w != coppervm.WordU64(10) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordI64(10), w)
+	}
+
+	w2 := casm.evaluateExpression(Expression{
+		Kind:          ExpressionKindNumLitFloat,
+		AsNumLitFloat: 2.3,
+	}, FileLocation{})
+	if w2 != coppervm.WordF64(2.3) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(2.3), w2)
+	}
+
+	w3 := casm.evaluateExpression(Expression{
+		Kind:        ExpressionKindStringLit,
+		AsStringLit: "str",
+	}, FileLocation{})
+	if w3 != coppervm.WordI64(0) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(0), w3)
+	}
+
+	w4 := casm.evaluateExpression(Expression{
+		Kind: ExpressionKindBinaryOp,
+		AsBinaryOp: BinaryOp{
+			Kind: BinaryOpKindPlus,
+			Lhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 2,
+			},
+			Rhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 5,
+			},
+		},
+	}, FileLocation{})
+	if w4 != coppervm.WordI64(7) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(7), w4)
+	}
+
+	w5 := casm.evaluateExpression(Expression{
+		Kind: ExpressionKindBinaryOp,
+		AsBinaryOp: BinaryOp{
+			Kind: BinaryOpKindMinus,
+			Lhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 2,
+			},
+			Rhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 5,
+			},
+		},
+	}, FileLocation{})
+	if w5 != coppervm.WordI64(-3) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(-3), w5)
+	}
+
+	w6 := casm.evaluateExpression(Expression{
+		Kind: ExpressionKindBinaryOp,
+		AsBinaryOp: BinaryOp{
+			Kind: BinaryOpKindTimes,
+			Lhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 2,
+			},
+			Rhs: &Expression{
+				Kind:        ExpressionKindNumLitInt,
+				AsNumLitInt: 5,
+			},
+		},
+	}, FileLocation{})
+	if w6 != coppervm.WordI64(10) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(10), w6)
+	}
+
+	func() {
+		defer func() { recover() }()
+		casm.evaluateExpression(Expression{
+			Kind:      ExpressionKindBinding,
+			AsBinding: "bind",
+		}, FileLocation{})
+		t.Error("expecting an error")
+	}()
+
+	casm.Bindings = append(casm.Bindings, Binding{
+		Name:  "a_bind",
+		Value: Expression{Kind: ExpressionKindNumLitInt, AsNumLitInt: 3},
+	})
+	w7 := casm.evaluateExpression(Expression{
+		Kind:      ExpressionKindBinding,
+		AsBinding: "a_bind",
+	}, FileLocation{})
+	if w7 != coppervm.WordI64(3) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordF64(3), w7)
+	}
+}
+
+func TestEvaluateBinding(t *testing.T) {
+	casm := Casm{
+		Bindings: []Binding{
+			{
+				Name: "a_bind",
+				Value: Expression{
+					Kind:        ExpressionKindNumLitInt,
+					AsNumLitInt: 5,
+				},
+			},
+			{
+				Name: "cycl1",
+				Value: Expression{
+					Kind:      ExpressionKindBinding,
+					AsBinding: "cycl2",
+				},
+			},
+			{
+				Name: "cycl2",
+				Value: Expression{
+					Kind:      ExpressionKindBinding,
+					AsBinding: "cycl1",
+				},
+			},
+		},
+	}
+
+	w := casm.evaluateBinding(&casm.Bindings[0], FileLocation{})
+	if w != coppervm.WordU64(5) {
+		t.Errorf("expecting %#v but got %#v", coppervm.WordU64(5), w)
+	}
+
+	// w2 := casm.evaluateBinding(&casm.Bindings[1], FileLocation{})
+	// if w2 != coppervm.WordU64(5) {
+	// 	t.Errorf("expecting %#v but got %#v", coppervm.WordU64(5), w2)
+	// }
 }
