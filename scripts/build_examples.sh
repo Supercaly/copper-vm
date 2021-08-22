47425c6c4c4b61cd72d9959fb122a27a31b6ceaf
Lorenzo <djonline96@gmail.com>
2021-08-22 18:27:47 +0200

    Move copper libraries to stdlib folder

--- scripts/build_examples.sh
+++ scripts/build_examples.sh
@@ -8,5 +8,5 @@ examples=$(find examples/ -maxdepth 1 -name "*.copper" -type f)
 for e in $examples; do
     name=$(basename $e)
     name=${name%.copper}
-    ./build/casm -o "examples/bin/$name.vm" $e -I examples/
+    ./build/casm -o "examples/bin/$name.vm" $e -I stdlib/
 done
